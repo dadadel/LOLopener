a0bfc87a91913a6872130664b1f9d1107856ae47
dadadel <daouzli@gmail.com>
2014-06-28 13:33:11 +0200

    testing IRC bot using PyIrciBot

---
+++ irc/lolopener.py
@@ -0,0 +1,89 @@
+from time import time
+from pyircibot import PyIrciBot
+
+
+class LolOpenerIrcBot(object):
+    '''LOLOpener IRC bot
+    '''
+
+    def __init__(self, nick=None, channel=None):
+        '''Init the data
+
+        '''
+        self.channel = channel
+        self.nick = nick
+        self.init_time = time()
+        self.is_open = False
+
+    def set_nick(self, nick):
+        '''Sets the nick. This will be called by PyIrciBot to update the nickname.
+
+        @param nick: the nick name
+        
+        '''
+        self.nick = nick
+
+    def set_channel(self, channel):
+        '''Sets the channel. This will be called by PyIrciBot to update the channel.
+
+        @param channel: the channel name
+        
+        '''
+        self.channel = channel
+
+    def parse_raw(self, data):
+        '''Proceeds raw data
+
+        '''
+        pass
+
+    def asked_if_open(self, message):
+        return 'ouvert ?' in message or 'ouvert?' in message or message.strip() == '?'
+
+    def open_message(self):
+        return 'Le LOLcal {}'.format('est ouvert !' if self.is_open else "n'est pas ouvert !")
+
+    def timeout_function(self):
+        # read GPIO status
+        # if changed status announce it to the channel
+        # self.is_open
+        return None
+
+    def parse_message(self, message, source, target):
+        '''Proceeds a message received with PRIVMSG.
+        Will react if the following are found in the message:
+            -'!bot: arrete toi stp': will send stop request to the bot
+            -'?' || 'ouvert ?' || 'ouvert?': send to the channel a message saying if the LOLcal is open or not
+
+        @param message: the message
+        @param source: the sender of the message
+        @param target: the target of the message (might be the channel or the bot)
+        @return: None or dictionary
+
+        '''
+        if not target.startswith('#'):
+            # Private message
+            if '!bot: arrete toi stp' in message:
+                print ("je m'arrete")
+                return {'cmd': {'end': '', 'message': "On me demande de m'arreter. Au revoir..."}}
+            if self.asked_if_open(message):
+                return {'cmd': {'message': self.open_message()}}
+            #TODO: remove when the GPIO will be used
+            if 'CHANGE' in message:
+                self.is_open = not self.is_open
+        else:
+            # Channel message
+            if message.startswith(self.nick) and not message.replace(self.nick, '')[0].isalnum():
+                if self.asked_if_open(message.replace(self.nick, '')[1:]):
+                    return {'cmd': {'message': self.open_message()}}
+            else:
+                pass
+ 
+if __name__ == '__main__':
+    server = "irc.lyonopenlab.net"
+    channel = "#testlol"
+    botnick = "lolopener"
+    bot = PyIrciBot(server, channel, botnick)
+    bot.connect(timeout_use_class=True)
+    bot.use_parser_class(LolOpenerIrcBot)
+    bot.run()#parse_message=parse_message)
