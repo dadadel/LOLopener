7e8981ae262aae1027ad44a9c7bd89e5480fc814
dadadel <daouzli@gmail.com>
2014-07-30 13:03:11 +0200

    fix MIME and addresses list

--- lolopener/lolmail.py
+++ lolopener/lolmail.py
@@ -1,25 +1,32 @@
+# -*- encoding: utf8 -*-
+
 import smtplib
-import email
+from email.mime.text import MIMEText
 
 from lolopenstatus import LOLOpenStatus
+
+message_open = """Le LOLcal est ouvert !
+Vous êtes donc les bienvenus
+
+Votre LOLOpener
+"""
+
+message_closed = """Le LOLcal est maintenant fermé !
+Désolé mais il faudra attendre qu'il réouvre avant de venir !
+
+Votre LOLOpener
+"""
 
 
 class LOLOpenerMail(object):
 
     def __init__(self, open_status=LOLOpenStatus()):
-        self.lolmsg = {True: """Le LOLcal est ouvert !
-        Vous êtes donc les bienvenus
-
-        Votre LOLOpener
-        """,
-                       False: """Le LOLcal est maintenant fermé !
-        Désolé mais il faudra attendre qu'il réouvre avant de venir !
-
-        Votre LOLOpener
-        """}
+        self.lolmsg = { True: message_open,
+                        False: message_closed
+                        }
         self.smtp_server = "localhost"
         self.from_addr = "lolopener@lolcal.lyonopenlab.net"
-        self.to_addr =  ["lyon-hackerspace@lists.hackerspaces.org"]
+        self.to_addr =  "lyon-hackerspace@lists.hackerspaces.org"
         self.subject = {True: "Le LOLcal est ouvert",
                         False: "Le LOLcal est fermé"}
         self.open_status = open_status
@@ -29,7 +36,7 @@ class LOLOpenerMail(object):
         while running:
             if self.open_status.update():
                 lol_open_status = self.open_status.is_open()
-                msg = email.mime.text.MIMEText(self.lolmsg[lol_open_status])
+                msg = MIMEText(self.lolmsg[lol_open_status], "plain", "utf8")
                 msg['From'] = self.from_addr
                 msg['To'] = self.to_addr
                 msg['Subject'] = self.subject[lol_open_status]
